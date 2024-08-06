d64df7ade65403d62e436c9f0076fe023edd772b
SolShark1 <tattooed112@yahoo.com>
2024-08-06 20:34:23 +0000

    update telegram_game_bot.py

--- telegram_game_bot.py
+++ telegram_game_bot.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext, Updater, ApplicationBuilder
+from telegram.ext import CommandHandler, CallbackContext, ApplicationBuilder
 
 # Load environment variables
 load_dotenv()

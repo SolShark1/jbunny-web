a1b3488153e1307b93cfe14fb1e39be6386f6d37
SolShark1 <tattooed112@yahoo.com>
2024-08-07 13:00:03 +0000

    update telegram_game_bot.py

--- telegram_game_bot.py
+++ telegram_game_bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Enable logging
 logging.basicConfig(
